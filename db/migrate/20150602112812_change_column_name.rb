aba5fdd0d5cfa56a52b013a924f488e1d23afc98
Stammel <hstammel@XIN837378.asv.local>
2015-06-11 10:33:15 +0200

    Changes

--- db/migrate/20150602112812_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration
-  def change
-  	#rename_column :sistrix_visibility_indices, :index, :sistrix_index
-  	change_column :sistrix_visibility_indices, :sistrix_index, :decimal
-  end
-end
--- db/migrate/20150602113702_change_column_type.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnType < ActiveRecord::Migration
-  def change
-
-  	change_column :sistrix_visibility_indices, :sistrix_index, :decimal
-  end
-end
-
